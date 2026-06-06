aa6ca747352760f5094c380671c055840e8c9a52
João vitor <silvajoaovitor341@gmail.com>
2026-06-05 22:08:47 -0300

    Update tarefachat.java

--- vetores/tarefachat.java
+++ vetores/tarefachat.java
@@ -7,7 +7,7 @@ public class tarefachat {
         int tamanho = sc.nextInt();
         String[] vetor = new String[tamanho];
         sc.nextLine();
-
+     
         for(int i = 0; i < vetor.length; i++){
             System.out.println("Digite a palavra na pocisão " + i + " do vetor: ");
             vetor[i]= sc.nextLine();
@@ -16,7 +16,7 @@ public class tarefachat {
              System.out.println(vetor[i]);
         }
 
-
+        //mostrar o maior nome 
         String maior = vetor[0];
         for(int i = 0; i < vetor.length; i++){
             if (vetor[i].length() > maior.length()){
@@ -24,7 +24,7 @@ public class tarefachat {
             }
         }
         System.out.println("O maior nome do vetor e: " + maior);
-
+        //contador de vogais
         int vogais = 0;
         for(int i = 0; i < vetor.length; i++){
             for(int j = 0; j < vetor[i].length(); j++){
@@ -35,7 +35,7 @@ public class tarefachat {
             }
         }
         System.out.println("A quantidade de vogais nos vetores e: " + vogais);
-
+        //mostrar valores que começam com uma letra especifica
         int iniciais = 0;
         for(int i = 0; i < vetor.length; i++){
             char letra = vetor[i].charAt(0);
@@ -45,7 +45,7 @@ public class tarefachat {
         }
         System.out.println("Nomes que começam com a letra A:" + iniciais);
 
-
+        // função para mostrar o maior nome invertido
         char[] nomemaior = maior.toCharArray();
         for(int i = 0; i < nomemaior.length / 2; i++){
             char aux =nomemaior[i];
